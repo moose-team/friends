348d15e2f38cf3923421cc2b114c51c7af041dca
Kyle Robinson Young <kyle@dontkry.com>
2015-04-26 21:30:13 -0700

    Update to view-list@1.2.0 which calculates height automatically and recalc height on window resize

--- app/elements/messages.js
+++ app/elements/messages.js
@@ -4,25 +4,16 @@ var h = require('virtual-dom/h')
 var ViewList = require('view-list')
 
 function Messages (app) {
+  var self = this
   this.app = app
-
-  // Create an onlooad hook that starts the list at the bottom
-  var Onload = function () {}
-  Onload.prototype.hook = function (node, propertyName, previousValue) {
-    // TODO: Still need a better way to call a hook after inserted into the DOM
-    // TODO: See https://github.com/Matt-Esch/virtual-dom/issues/233
-    setTimeout(function () {
-      node.scrollTop = node.scrollHeight
-    }, 0)
-  }
 
   this.viewList = new ViewList({
     className: 'messages',
-    onload: new Onload(),
+    // TODO: Calculate this rowHeight automatically as well
+    rowHeight: 42,
     eachrow: function (msg) {
       // TODO: this is insecure. Need to only show this when public key is verified
       var verified = !/Anonymous/i.test(msg.username)
-
       return h('li.message.clearfix', [
         h('img.avatar', { src: msg.avatar }),
         h('.username', msg.username),
@@ -32,13 +23,23 @@ function Messages (app) {
       ])
     }
   })
+
+  // On window resize, adjust the height for viewList calculations
+  window.addEventListener('resize', function (e) {
+    var messages = document.querySelector('.messages')
+    self.viewList.height = messages.offsetHeight
+  }, false)
 }
 
 Messages.prototype.render = function (messages) {
+  var childViews
+
   if (messages.length === 0) {
     var starterMessage = 'This is new channel. Send a message to start things off!'
-    return h('.messages.starterMessage', this, starterMessage)
+    childViews = h('.messages.starterMessage', this, starterMessage)
+  } else {
+    childViews = this.viewList.render(messages)
   }
 
-  return this.viewList.render(messages)
+  return h('.messages-container', childViews)
 }
