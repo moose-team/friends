21214507de15437c72121d46979dbd50df93402e
Kyle Robinson Young <kyle@dontkry.com>
2015-04-26 19:54:13 -0700

    Implement view-list for messages

--- app/elements/messages.js
+++ app/elements/messages.js
@@ -2,9 +2,31 @@ module.exports = Messages
 
 var h = require('virtual-dom/h')
 var moment = require('moment')
+var ViewList = require('view-list')
 
 function Messages (app) {
   this.app = app
+
+  // Create an onlooad hook that starts the list at the bottom
+  var Onload = function () {}
+  Onload.prototype.hook = function (node, propertyName, previousValue) {
+    setTimeout(function () {
+      node.scrollTop = node.scrollHeight
+    }, 0)
+  }
+
+  this.viewList = new ViewList({
+    className: 'messages',
+    onload: new Onload(),
+    eachrow: function(msg) {
+      return h('li.message.clearfix', [
+        h('img.avatar', { src: msg.avatar }),
+        h('.username', msg.username),
+        h('.timestamp', moment(msg.timestamp).fromNow()),
+        h('.text', msg.text)
+      ])
+    }
+  })
 }
 
 Messages.prototype.render = function (messages) {
@@ -13,13 +35,5 @@ Messages.prototype.render = function (messages) {
     return h('.messages.starterMessage', this, starterMessage)
   }
 
-  messages = messages.map(function (msg) {
-    return h('.message.clearfix', [
-      h('img.avatar', { src: msg.avatar }),
-      h('.username', msg.username),
-      h('.timestamp', moment(msg.timestamp).fromNow()),
-      h('.text', msg.text)
-    ])
-  })
-  return h('.messages', messages)
+  return this.viewList.render(messages)
 }
