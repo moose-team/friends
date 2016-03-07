eafe4a4fe380cabf2e2b5e8e9fe890f7286f5852
Nate Goldman <nate@ngoldman.me>
2016-03-07 09:17:57 -0800

    make packaging work cross-platform

--- pkg.js
+++ pkg.js
@@ -7,9 +7,9 @@ var sh = require('shelljs')
 
 var appVersion = pkgjson.version
 var appName = pkgjson.name
-var electronPackager = './node_modules/.bin/electron-packager'
+var electronPackager = path.join('node_modules', '.bin', 'electron-packager')
 var electronVersion = pkgjson.devDependencies['electron-prebuilt']
-var icon = 'static/Icon.icns'
+var icon = path.join('static', 'Icon.icns')
 
 if (process.argv[2] === '--all') {
   // build for all platforms
@@ -27,22 +27,28 @@ if (process.argv[2] === '--all') {
 }
 
 function pack (plat, arch) {
-  var outputPath = path.join('.', 'pkg', appVersion, plat, arch)
-
-  sh.exec('./node_modules/.bin/rimraf ' + outputPath)
+  var prefix = os.platform() === 'win32' ? '.\\' : './'
+  var rimraf = path.join('node_modules', '.bin', 'rimraf')
+  var outputPath = path.join('pkg', appVersion, plat, arch)
 
   // there is no darwin ia32 electron
   if (plat === 'darwin' && arch === 'ia32') return
 
-  var cmd = electronPackager + ' . ' + appName +
-    ' --platform=' + plat +
-    ' --arch=' + arch +
-    ' --version=' + electronVersion +
-    ' --app-version' + appVersion +
-    ' --icon=' + icon +
-    ' --out=' + outputPath +
-    ' --prune' +
-    ' --ignore=pkg' // ignore the pkg directory or hilarity will ensue
-  console.log(cmd)
-  sh.exec(cmd)
+  var cmd1 = `${prefix}${rimraf} ${outputPath}`
+  var cmd2 = `${prefix}${electronPackager} . ${appName} ` +
+    `--platform=${plat} ` +
+    `--arch=${arch} ` +
+    `--version=${electronVersion} ` +
+    `--app-version=${appVersion} ` +
+    `--icon=${icon} ` +
+    `--out=${outputPath} ` +
+    `--prune ` +
+    `--ignore=pkg` // ignore the pkg directory or hilarity will ensue
+
+  console.log(`${cmd1}\n${cmd2}`)
+
+  if (process.argv.slice(2)[0] === '--dry') process.exit(0)
+
+  sh.exec(cmd1)
+  sh.exec(cmd2)
 }
